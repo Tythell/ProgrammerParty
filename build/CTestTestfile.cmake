469ea24163b7ffb195cd27531ab1dd065068696a
Ming <mail@zyming.me>
2024-11-15 13:15:45 +0100

    Add gitignore and remove ./build/

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/christian/ProgrammerParty
-# Build directory: /home/christian/ProgrammerParty/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/christian/ProgrammerParty/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
