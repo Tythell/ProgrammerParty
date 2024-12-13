67a3488f2a4eafb481e765332cbdc5025b44033d
Ming <mail@zyming.me>
2024-12-13 13:46:37 +0100

    sync repo for next programming practice

--- AocDay4Test.cc
+++ AocDay4Test.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <cstdint>
 #include <string>
+#include <sstream>
 
 /*
 Exercise suggestion for programming practice tomorrow
@@ -16,15 +17,57 @@ and count the number of occurrences
 Lore text here:S
 */
 
-uint32_t countXmas(const std::string& input) {
+uint8_t NextLetterDown(const std::string& input, int32_t currentXPos, int32_t currentYPos)
+{
+    std::vector<std::string> bufferMap;
+    std::stringstream ss(input);
+
+    std::string line;
+    while (std::getline(ss, line))
+    {
+        bufferMap.emplace_back(line);
+    }
+    
+
+}
+
+uint32_t countWordDownward(const std::string& input, const std::string& word)
+{
+    const auto lineLength = input.find('\n');
+    if (lineLength == std::string::npos)
+    {
+        return 0u;
+    }
+    
+    size_t pos = input.find(word[0u]);
+    while (pos < input.size() - (3 * lineLength))
+    {
+    find 'X' until end of input
+    if input[pos of 'X' + lineLength] == word[0u] and
+       input[pos of 'X' + 2 * lineLength] == word[1u] and ...
+       count++
+
+    }
+
+}
+
+uint32_t countWord(const std::string& input, const std::string& word)
+{
     std::uint32_t count{0u};
-    size_t offset = input.find("XMAS");
+    size_t offset = input.find(word);
     while (offset != std::string::npos)
     {
         ++count;
-        offset = input.find("XMAS", offset + 4u);
+        offset = input.find(word, offset + 4u);
     }
     return count;
+}
+
+uint32_t countXmas(const std::string& input) {
+    return countWord(input, "XMAS") + 
+    countWord(input, "SAMX") + 
+    countWordDownward(input, "XMAS") + 
+    countWordDownward(input, "SAMX");
 }
 
 TEST(Day4Test, DISABLED_verify_problem1Solution) {
@@ -56,5 +99,41 @@ TEST(Day4Test, noXMAS){
 
 TEST(Day4Test, twoXMAS){
     EXPECT_EQ(2, countXmas("XMASXMAS"));
+}
+
+TEST(Day4Test, twoLinesXmas){
+    ASSERT_EQ(2, countXmas("XMAS\nXMAS"));
+}
+
+TEST(Day4Test, FiveXMasMultiline){
+    ASSERT_EQ(5, countXmas("XMASXMASXMAS\nXMASXMASMMMM"));
+}
+
+TEST(Day4Test, xmasBackwards){
+    ASSERT_EQ(1, countXmas("SAMX"));
+}
+
+TEST(Day4Test, xmasBackwardsForwards){
+    ASSERT_EQ(2, countXmas("XMASAMX"));
+}
+
+TEST(Day4Test, xmasForwardsBackwards){
+    ASSERT_EQ(2, countXmas("SAMXMAS"));
+}
+
+TEST(Day4Test, xmasDown) {
+    constexpr auto input = "X\n"
+                           "M\n"
+                           "A\n"
+                           "S";
+    ASSERT_EQ(1, countXmas(input));
+}
+
+TEST(Day4Test, DISABLED_xmasDiagonalDownRight) {
+    constexpr auto input = "X...\n"
+                           ".M..\n"
+                           "..A.\n"
+                           "...S";
+    ASSERT_EQ(1, countXmas(input));
 }
 
