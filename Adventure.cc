5c5ee70ecc05a8c3d03d23776eaec6e60fdaf8a5
Tythell <christianfalk781@gmail.com>
2025-01-30 15:59:24 +0100

    Fixed AdventureGameTest_printTheMapFromFile + Added index comments to desired buffer because counting dots everytime I want to find a coord makes me want to throw furniture. * Map printout format is now "* " instead of "**" to make it more readable and still look like a square

--- Adventure.cc
+++ Adventure.cc
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <fstream>
 
 struct Coordinate
 {
@@ -33,11 +34,11 @@ public:
     {
         switch (m_type)
         {
-            case TileE::Empty : return "..";
-            case TileE::Adventurer : return "oo";
-            case TileE::Monster : return "@@";
-            case TileE::Treasure : return "**";
-            case TileE::Trap : return "##";
+            case TileE::Empty : return ". ";
+            case TileE::Adventurer : return "o ";
+            case TileE::Monster : return "@ ";
+            case TileE::Treasure : return "* ";
+            case TileE::Trap : return "# ";
         }
         std::terminate();
     }
@@ -74,16 +75,30 @@ public:
 
     void print() const
     {
+        // for(const auto& row : m_map)
+        // {
+        //     for(const auto& tile : row)
+        //     {
+        //         std::cout << tile;
+        //     }
+        //     std::cout << std::endl;
+        // }
+        std::cout << toString();
+    }
+    std::string toString() const
+    {
+        std::stringstream ss;
         for(const auto& row : m_map)
         {
             for(const auto& tile : row)
             {
-                std::cout << tile;
+                ss << tile;
             }
-            std::cout << std::endl;
+            ss << std::endl;
         }
+        return ss.str();
     }
-
+    
 private:
     std::vector<std::vector<Tile>> m_map;
 };
@@ -125,52 +140,56 @@ public:
         m_traps = traps;
         return *this;
     }
+    
+    Dungeon parseFromFile(const std::string& path)
+    {
+        std::ifstream reader(path);
+        assert(reader.is_open());
+        return buildFromStream(reader);
+    }
 
     Dungeon parseFromString(const std::string& input)
     {
-        // dimension_x dimension_y
-        // startPosition_x startPosition_y
-        // treasure_x treasure_y
-        // monsterPos_x monsterPos_y
-        // trap_x trap_y ..........
-        std::vector<Coordinate> readCoords;
-
         std::stringstream ss(input);
-        m_dimensions = toCoordinate(ss);
-        m_startPosition = toCoordinate(ss);
-        m_treasure = toCoordinate(ss);
-        m_monster = toCoordinate(ss);
-
-        while (auto trap = toCoordinate(ss); trap.has_value())
-        {
-         
-            m_traps.push_back(trap);
-        }
-        for (std::string line; std::getline(ss, line);)
-        {
-            readCoords.emplace_back(toCoordinate(line)); 
-        }
-
-        m_dimensions = readCoords.at(0);
-
-        return Dungeon{m_dimensions, readCoords.at(1), readCoords.at(3), readCoords.at(2), {readCoords.begin() + 4, readCoords.end()}};
+        return buildFromStream(ss);
     }
 
 private:
-    Coordinate toCoordinate(const std::string& line) const
+    template<typename Stream>
+    Dungeon buildFromStream(Stream& reader)
+    {
+        m_dimensions = toCoordinate(reader).value();
+        m_startPosition = toCoordinate(reader).value();
+        m_treasure = toCoordinate(reader).value();
+        m_monster = toCoordinate(reader).value();
+
+        auto trap = toCoordinate(reader);
+        while (trap.has_value())
+        {
+            m_traps.push_back(trap.value());
+            trap = toCoordinate(reader);
+        }
+        return build();
+    }
+
+    template<typename Stream>
+    std::optional<Coordinate> toCoordinate(Stream& ss) const
+    {
+        std::string line;
+        if (std::getline(ss, line))
+        {
+            return toCoordinateStr(line); 
+        }
+        return std::nullopt;   
+    }
+
+    Coordinate toCoordinateStr(const std::string& line) const
     {
          std::stringstream subss(line);
             std::uint32_t readNumberX, readNumberY;
             subss >> readNumberX;
             subss >> readNumberY;
             return {readNumberX, readNumberY}; 
-    }
-    Coordinate toCoordinate(std::stringstream& ss) const
-    {
-        std::uint32_t readNumberX, readNumberY;
-        ss >> readNumberX;
-        ss >> readNumberY;
-        return {readNumberX, readNumberY}; 
     }
 
     Coordinate m_dimensions{10u, 10u};
@@ -196,21 +215,31 @@ TEST(AdventureGameTest, printTheMap)
 TEST(AdventureGameTest, printTheMapFromText)
 {
     const auto map = DungeonBuilder().parseFromString("10 11\n0 3\n7 8\n5 5\n6 8\n7 9");
-/*                        setDimensions(10u, 10u).
-                        setStartPosition(0u, 3u).
-                        setMonster(5u, 5u).
-                        setTreasure(7u, 8u).
-                        setTraps({{6u, 8u}, {7u, 9u}}).
-                        build();*/
     map.print();
 }
-// ....................
-// ....................
-// ..................@@
-// ....................
-// ....##..............
-// ......##..##........
-// ....................
-// ..##..**..##........
-// ....................
-// oo....##............
+
+TEST(AdventureGameTest, printTheMapFromFile)
+{
+    const auto map = DungeonBuilder().parseFromFile("input.txt");
+    std::string desiredMap = 
+//   0 1 2 3 4 5 6 7 8 9
+    ". . . . . . . . . . \n" // 0
+    ". . . . . . . . . . \n" // 1
+    ". . . . . . . . . . \n" // 2
+    "o . . . . . . . . . \n" // 3
+    ". . . . . . . . . . \n" // 4
+    ". . . . . @ . . . . \n" // 5
+    ". . . . . . . . . . \n" // 6
+    ". . . . . . . . . . \n" // 7
+    ". . . . . . # * . . \n" // 8
+    ". . . . . . . # . . \n" // 9
+    ". . . . . . . . . . \n";// 10
+
+    EXPECT_EQ(desiredMap, map.toString());
+
+    // TODO: compare print output to map
+    std::cout << "---------- PrintMapFromFile ----------\n";
+    std::cout << desiredMap << std::endl;
+    std::cout << map.toString() << std::endl;
+    //map.print();
+}
